73545078763ab70595217d9ccc712fc8448e926c
DiegoAG1324 <97371456+DiegoAG1324@users.noreply.github.com>
2022-08-22 10:04:45 -0600

    tarea 4

---
+++ LUNES 15/arreglo 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESTRUCTURA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arreglo = new int[10];
+            int i;
+
+            for (i = 0; i < 10; i++)
+            {
+                arreglo[i] = 1;
+            }
+
+            for (i = 0; i < 10[]; i++)
+            {
+                Console.WriteLine("Datos del arreglo[" + i + "]: " + arreglo[i]);
+
+            }
+
+            Console.Read();
+        }
+    }
+}
